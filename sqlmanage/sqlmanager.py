2c4e6a94be9c6cb0233f9684bf86b0caf778811b
longlh <lehoanglong95@gmail.com>
2018-11-02 22:56:50 +0700

    ddd

--- sqlmanage/sqlmanager.py
+++ sqlmanage/sqlmanager.py
@@ -35,6 +35,6 @@ class SqlManager:
                  results.append(result)
          return results
 
-     def update_request_status(id, link_download,number_import, success_import):
+     def update_request_status(self, id, link_download,number_import, success_import):
          print("a")
          self.sql_helper.update_request_status(id, link_download, number_import, success_import)
